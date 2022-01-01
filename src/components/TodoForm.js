d8646862b2a83704c9c98a8efed7f56840f7d17f
Gulam Anas <g.anas786@gmail.com>
2022-01-01 22:21:44 +0530

    todo function created

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
     const [input, setInput] = useState('')
 
     const handleChange = (e) => {
@@ -10,10 +10,10 @@ function TodoForm() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // props.onSubmit({
-        //     id: Math.floor(Math.random() * 10000),
-        //     text: input
-        // })
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
 
         setInput('')
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,21 @@ import TodoForm from './TodoForm'
 function TodoList() {
     const [todos, setTodos] = useState([])
 
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+        }
+        // /^\s*$/.test(todo.text) is used to check is it empty or not
+
+        const newTodos = [todo, ...todos];
+
+        setTodos(newTodos);
+    }
+
     return (
         <div>
             <h1>What's the plan for Today?</h1>
-            <TodoForm />
+            <TodoForm onSubmit={addTodo} />
         </div>
     )
 }
